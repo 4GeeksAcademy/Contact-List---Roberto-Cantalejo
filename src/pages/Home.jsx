90e0615d96320b3ee160a88e09e6fa9d8b5739f8
Roberto <robercantalejo@gmail.com>
2025-02-17 20:25:01 +0000

    He hecho la base de lo que será el Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,15 @@ export const Home = () => {
   const {store, dispatch} =useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="agenda-container">
+			<div className="agenda">
+				<div className="agenda-pages">
+					<div className="agenda-left-page"></div>
+					<div className="agenda-right-page"></div>
+				</div>
+				<div className="agenda-split"></div>
+				<div className="agenda-marker"></div>
+				</div>
 		</div>
 	);
 }; 
